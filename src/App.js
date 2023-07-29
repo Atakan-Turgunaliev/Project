d01a590836ee491987d35348da5562ece2e57e3d
Пользователь <you@example.com>
2023-07-29 14:25:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import HomePage from './pages/HomePage/HomePage';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items"
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Футбольная форма',
+          img: 'soccer uniform.jpg',
+          desc: 'Футбольная форма Nike',
+          category: 'football clothes',
+          price: '39.99'
+        },
+        {
+          id: 2,
+          title: 'Тренировочный спортивный костюм',
+          img: 'Тренировочный спортивный костюм.jpg',
+          desc: 'Тренировочный спортивный костюм Barcelona Nike',
+          category: 'football clothes',
+          price: '49.99'
+        },
+        {
+          id: 3,
+          title: 'Футбольные бутсы',
+          img: 'бутсы.jpg',
+          desc: 'Футбольная форма Nike',
+          category: 'football shoes',
+          price: '119.99'
+        },
+        {
+          id: 4,
+          title: 'Капитанская повязка',
+          img: 'kapitanskajapovjazkared.jpg',
+          desc: 'Капитанская повязка Nike',
+          category: 'football accessories',
+          price: '9.99'
+        },
+        {
+          id: 5,
+          title: 'Щитки футбольные',
+          img: 'Щитки футбольные.jpg',
+          desc: 'Щитки футбольные Nike',
+          category: 'defense',
+          price: '19.99'
+        },
+        {
+          id: 6,
+          title: 'Футбольный мяч',
+          img: 'Футбольный мяч.jpg',
+          desc: 'Футбольный мяч Nike',
+          category: 'soccer ball',
+          price: '25.99'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+
+    let [cartOpen, setCartOpen] = useState(false)
+
+    return (
+        <header>
+            <div>
+                <span className='logo'>Mens football clothing</span>
+                <ul className='nav'>
+                    <li>Про нас</li>
+                    <li>Контакты</li>
+                    <li>Кабинет</li>
+                </ul>
+                <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+                {cartOpen && (
+                    <div className='shop-cart'>
+                    </div>
+                )}
+            </div>
+            <div className='presentation'></div>
+        </header>
+    );
+};
+
+export default Header;
